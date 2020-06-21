e8963f647c21faa01777e3fef78d42e057600448
Marek Topolewski <mtopolew@cisco.com>
2020-06-21 20:21:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# mniam! SMS sending service
+
+### Prerequisites
+
+- A compatible Huawei modem (for a list see https://github.com/Salamek/huawei-lte-api)
+
+- A python3 interpreter
+
+### Usage
+
+- Clone this repo: `git clone https://github.com/marektopolewski/mniam-sms`
+
+- Install python dependencies: `pip install -r requirements.txt.`
+
+- Start the service: `./server.py` (or `./server.py -` to run in the background)
---
+++ server.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import sys
+import sched
+import time
+import requests
+import json
+from huawei_lte_api.Client import Client
+from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
+from huawei_lte_api.exceptions import ResponseErrorLoginRequiredException
+
+class Sender:
+
+    def __init__(self, backend_url : str):
+        self._client = self.make_client()
+        self._queue = dict()
+        self._scheduler = sched.scheduler(time.time, time.sleep)
+        self._backend_url = backend_url
+
+    ############ CREATE A huawei_lte_api CLIENT ############
+    def make_client(self):
+        MODEM_CREDS_USERNAME = "admin"
+        MODEM_CREDS_PASSWORD = "xxxxxxx"
+        MODEM_LOCAL_IP = "192.168.8.2"
+
+        # establish a connection
+        connection = AuthorizedConnection('http://{}:{}@{}/'.format(MODEM_CREDS_USERNAME,
+                                                                    MODEM_CREDS_PASSWORD,
+                                                                    MODEM_LOCAL_IP))
+        # init the client
+        return Client(connection)
+
+    ############ WRAPPER FOR SEND SMS + SEND CONFIRMATION ############
+    def handle_request(self, sms_id: str, phone_number: str, message: str):
+        try:
+            err = self.send_sms([phone_number], message)
+            self.confirm_sent(sms_id, not err)
+        except Exception as e:
+            print("[HANDLER] Handle request error : {}".format(e))
+
+    ############ SEND SMS WITH USING THE MODEM CLIENT ############
+    def send_sms(self, phone_numbers: list, message: str):
+        try:
+            response = self._client.sms.send_sms(phone_numbers, message)
+            if response != "OK":
+                return "Could not send ({}): {} => {}".format(response, phone_numbers, message)
+        except ResponseErrorLoginRequiredException:
+            self._client = self.make_client()
+
+    ############ TELL THE MNIAM BACKEND THAT SMS WAS SENT ############
+    def confirm_sent(self, sms_id: str, sent: bool):
+        data = {'sms_id': sms_id,
+                'status': "OK" if sent else "ERR"}
+        headers = {'content-type': 'application/json'}
+        r = requests.post(self._backend_url + "api/sms/update", headers=headers, data=json.dumps(data))
+        print(r.status_code, r.content)
+
+    ############ CALLED EACH LOOP IT INVOCATION ############
+    def _run(self, sc):
+        try:
+            # fetch list
+            r = requests.get(self._backend_url + "api/sms/fetch")
+            try:
+                # decode and handle send requests
+                blob = r.json()
+                if blob["message"] == "OK":
+                    for [sms_id, sms_data] in blob["queue"].items():
+                        self.handle_request(sms_id, sms_data["phone_number"], sms_data["message"])
+            except:
+                print("[LOOP] JSON error : could not decode.\n Code: {}\n Content: {}"
+                    .format(r.status_code, r.content))
+        except:
+            print("[LOOP] Request error : could not get /api/sms/fetch")
+        self._scheduler.enter(60, 1, self._run, (sc,))
+
+    ############ STARTS THE INFINITE LOOP ############
+    def serve(self):
+        self._scheduler = sched.scheduler(time.time, time.sleep)
+        self._scheduler.enter(0, 1, self._run, (self._scheduler,))
+        self._scheduler.run()
+
+if __name__ == "__main__":
+    Sender("http://localhost:3000/").serve()
